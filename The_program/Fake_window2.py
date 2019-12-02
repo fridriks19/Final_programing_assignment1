9b20cde7a17d8986d674673433934696a7c7e221
fridrik <fridriks19@ru.is>
2019-12-02 12:00:44 +0000

    Update Fake_window2.py

--- The_program/Fake_window2.py
+++ The_program/Fake_window2.py
@@ -7,6 +7,7 @@ CHANGE = "'1' - Breyta"
 MAKE = "'2' - Nýskrá"
 GET = "'3' - Sækja"
 QUIT = "'q' - Hætta"
+GO_BACK = "'r' - Til baka"
 PICK = "Veldu skipun:"
 USER_INPUT = ("Valin skipun: ")
 EMPLOYEE = "'1' - Starfmann"
@@ -33,11 +34,11 @@ while user != "q":
     
     if user == "1":
         user1 = user 
-        while user1 != "q":
+        while user1 != "r":
             print(BORDER * WITDH +"\n" + int(HEADER_MID)*" " +  HEADER  +   "\n" + BORDER * WITDH )
   
             print(PICK +"\n")
-            print(QUIT+ "\n")
+            print(QUIT+ " "*5 + GO_BACK +"\n")
             print(EMPLOYEE)
             print(PLACE)
             print(WORK_FLIGHT)
@@ -48,27 +49,27 @@ while user != "q":
 
     elif user == "2":
         user2 = user 
-        while user2 != "q":
+        while user2 != "r":
             print(BORDER * WITDH +"\n" + int(HEADER_MID)*" " +  HEADER  +   "\n" + BORDER * WITDH )
   
             print(PICK +"\n")
-            print(QUIT+ "\n")
+            print(QUIT+ " "*5 + GO_BACK +"\n")
             print(EMPLOYEE)
             print(PLACE)
             print(WORK_FLIGHT)
             print(AIRPLANE)
             print(AIRPLANE_TYPE)
             print()
-            user = input(USER_INPUT)
+            user2 = input(USER_INPUT)
             print()
 
     elif user == "3":
         user3 = user 
-        while user3 != "q":
+        while user3 != "r":
             print(BORDER * WITDH +"\n" + int(HEADER_MID)*" " +  HEADER  +   "\n" + BORDER * WITDH )
   
             print(PICK +"\n")
-            print(QUIT+ "\n")
+            print(QUIT+ " "*5 + GO_BACK +"\n")
             print(EMPLOYEE)
             print(PLACE)
             print(WORK_FLIGHT)
