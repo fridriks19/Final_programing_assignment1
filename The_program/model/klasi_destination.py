2f1035240bf32b85db357d7e6a122ddca4d83c99
fridrik <fridriks19@ru.is>
2019-12-03 14:40:37 +0000

    s

--- The_program/model/klasi_destination.py
+++ The_program/model/klasi_destination.py
@@ -1,8 +1,14 @@
 class Destination():
     def __init__(self, destination_info_list):
         self.destination_info_list = destination_info_list
-        self.destinationID = self.destination_info_list[0]
-        self.destinationName = self.destination_info_list[1]
+        self.__destinationId = self.destination_info_list[0]
+        self.__destinationName = self.destination_info_list[1]
     
     def __str__(self):
         return "{}".format(self.destination_info_list)
+    
+    def get_destinationId(self):
+        return self.__destinationId
+
+    def get_destinationName(self):
+        return self.__destinationName
