2061a7c8e839b900f325d83c5c4e6c00ccd042f8
Eggert <eggerth19@ru.is>
2019-12-03 14:29:25 +0000

    save/load employee

---
+++ The_program/klasiemployee.py
@@ -0,0 +1,20 @@
+class Starfsmadur:
+
+    def __init__(self, empl_info_list):
+        #self.stm_dict = {}
+        self.empl_info_list = empl_info_list
+        self.kennitala = self.empl_info_list[0]
+        self.nafn = self.empl_info_list[1]
+        self.starfsheiti = self.empl_info_list[2]
+        self.stodugildi = self.empl_info_list[3]
+        self.rettindi = self.empl_info_list[4]
+        self.heimilisfang = self.empl_info_list[5]
+        self.gsmsimi = self.empl_info_list[6]
+        self.val = self.empl_info_list[0:7]
+        #self.val.pop(1)
+        #self.stm_dict[self.nafn] = self.val      
+  
+    def __str__(self):
+        #return "{}".format(self.stm_dict)
+        return "{}".format(self.empl_info_list)
+
