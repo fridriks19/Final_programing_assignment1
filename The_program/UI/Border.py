8a3e4fd527bb16c079649994d08f0cfc69e8e078
fridrik <fridriks19@ru.is>
2019-12-03 13:43:33 +0000

    s

---
+++ The_program/UI/Border.py
@@ -0,0 +1,50 @@
+class Menu():
+    def __init__(self):
+        self.imorpta eh random
+    
+    if user_input == "Main menu":
+
+    if user_input == "Make menu":
+
+    if user_input == "Change menu":
+
+    if user_input == get
+
+
+
+
+******************************************************************************
+Starfsmenn = "1", Flugvélar = "2", Vinnuferðir = "3", Áfangastaðir = "4"     *
+******************************************************************************
+        Veldu skipun:__
+   
+            NAN AIR 
+
+        Valinn skipun:_
+
+******************************************************************************
+
+
+
+******************************************************************************
+Starfsmenn = "1", Flugvélar = "2", Vinnuferðir = "3", Áfangastaðir = "4"     *
+******************************************************************************
+        Veldu skipun:__
+   
+1- sækja 
+2- breyta 
+3- skrá nýjan starfsman 
+
+        Valinn skipun:_
+
+******************************************************************************
+
+
+
+if user_input == "Employees"
+
+if user_input == "Airplanes"
+
+if user_input == "Work flights"
+
+if user_input == "Destination"
