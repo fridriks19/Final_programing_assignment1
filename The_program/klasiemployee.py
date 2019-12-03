34182d61920d8f4e5171391190d37699e3791693
Eggert <eggerth19@ru.is>
2019-12-03 11:29:39 +0000

    unnið meira í klösum

--- The_program/klasiemployee.py
+++ The_program/klasiemployee.py
@@ -3,21 +3,20 @@ class Starfsmadur:
     def __init__(self, empl_info_list):
         self.stm_dict = {}
         self.empl_info_list = empl_info_list
-        self.nafn = self.empl_info_list[0]
-        self.kennitala = self.empl_info_list[1]
-        self.heimilisfang = self.empl_info_list[2]
-        self.heimasimi = self.empl_info_list[3]
-        self.gsmsimi = self.empl_info_list[4]
-        self.netfang = self.empl_info_list[5]
-        self.val = self.empl_info_list[1:6]
+        self.kennitala = self.empl_info_list[0]
+        self.nafn = self.empl_info_list[1]
+        self.starfsheiti = self.empl_info_list[2]
+        self.stodugildi = self.empl_info_list[3]
+        self.rettindi = self.empl_info_list[4]
+        self.heimilisfang = self.empl_info_list[5]
+        self.gsmsimi = self.empl_info_list[6]
+        self.val = self.empl_info_list[0:7]
+        self.val.pop(1)
         self.stm_dict[self.nafn] = self.val      
   
     def __str__(self):
-        prnt_str = ""
-        prnt_str += self.nafn + "\n"
-        for i in self.val:
-            prnt_str += i + "\n"
-            #if self.val.index(i) < (len(self.val)-1):
-            #    prnt_str += "\n"
-        return prnt_str
+        #return "{}".format(self.stm_dict)
+        return "{}".format(self.stm_dict)
+
+
 
--- The_program/klasiflugmadur.py
+++ The_program/klasiflugmadur.py
@@ -1,19 +1,6 @@
 from klasiemployee import Starfsmadur
 
 class Flugmadur(Starfsmadur):
-    def __init__(self, empl_info_list, stodugildi, rettindi):
+    def __init__(self, empl_info_list):
         Starfsmadur.__init__(self, empl_info_list)
-        self.starfstitill = "Pilot"
-        self.stodugildi = stodugildi
-        self.rettindi = rettindi
-
-    def __str__(self):
-        prnt_str = Starfsmadur.__str__(self)
-        prnt_str += self.starfstitill + "\n"
-        prnt_str += self.stodugildi + "\n"
-        prnt_str += self.rettindi + "\n"
-        return prnt_str
-
-list_1 = ["Eggert Orri Hermannsson", "290688-4189", "Dalsgerði", "865-8995", "865-8995", "eggerth19@ru.is"]
-sm1 = Flugmadur(list_1, "Captain", "NAFokkerF100")
-print(sm1)
+        self.val[1] = "Pilot"
