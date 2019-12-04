61359049f39a9d127df526fd7459f0e1d4f9b7b8
DavidMatt1 <54944682+DavidMatt1@users.noreply.github.com>
2019-12-04 11:02:32 +0000

    Update class_employeeIO.py

--- The_program/class_employeeIO.py
+++ The_program/class_employeeIO.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD:The_program/klasistarfsmadurIO.py
 empl_str = "1103647756,Wilma Horne,Cabincrew,Flight Attendant,N/A,Fellsmúli 25,8998825"
-=======
-from class_employee import employee
-from class_pilot import pilot
-
-
-empl_str = "111111-4189,Ekki Eggert Orri Hermannsson,Pilot,Main-Pilot,Jumbo999,Funalind,865-8996"
->>>>>>> 8ac353a2232c5c2d80021f7974522c3a8255a294:The_program/class_employeeIO.py
 
 class employeeIO:
     def __init__(self, empl_str):
@@ -32,10 +24,5 @@ class employeeIO:
                 return employee
             return "Starfsmaður fannst ekki"
 
-<<<<<<< HEAD:The_program/klasistarfsmadurIO.py
-s1 = StarfsmadurIO(empl_str)
+s1 = employeeIO(empl_str)
 print(s1.load_employee())
-=======
-S1 = employeeIO(empl_str)
-print(S1.load_employee())
->>>>>>> 8ac353a2232c5c2d80021f7974522c3a8255a294:The_program/class_employeeIO.py
