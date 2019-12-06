3a09587056e55f7f4d6cfb8d4aad12293ff40046
Eggert <eggerth19@ru.is>
2019-12-06 10:35:14 +0000

    e1

--- repo/class_EmployeeRepository.py
+++ repo/class_EmployeeRepository.py
@@ -1,9 +1,5 @@
 from models.class_employee import Employee
-#from models.class_employee import Employee
-#from class_pilot import pilot
 
-#empl_str = "111111-4189"
-#empl_str = ["1111114189","Ekki Eggert Orri Hermannsson","Pilot","Main-Pilot","Jumbo999","Funalind","865-8996"]
 
 class EmployeeRepository:
     def __init__(self, empl_str =""):
@@ -48,26 +44,12 @@ class EmployeeRepository:
         open_file = open("./data/crew2.csv" , "r")
         for line in open_file:
             line = line.split(",")
-            crew_dictionary[line[1]] = line    # make the name the key and the values the rest of line
+            crew_dictionary[line[1]] = line    # Make the name the key and the values the rest of line
         
         crew_dictionary[name][choice] = change
-        for key in crew_dictionary.keys():  # go through all the values so we can add them to a new string 
+        for key in crew_dictionary.keys():  # Go through all the values so we can add them to a new string 
             new_file += ",".join(crew_dictionary[key])  
         open_file = open("./data/crew2.csv", "w")  #We replace the old crew file with the new file 
         open_file.write(new_file)
         open_file.close()
         return "Upplýsingum breytt"
-        
-
-    
-
-# change = "Ananas"  # breytingin
-# choice = 5   # hverju á að breyta 
-# name = "Ekki Eggert Orri Hermannsson"  
-
-# S1 = EmployeeRepository(empl_str)
-# print(S1.add_employee())
-#print(S1.get_employee())
-#print(S1.change_employee(choice, change, name))
-#s1 = EmployeeRepository()
-#print(s1.get_allemployees())
