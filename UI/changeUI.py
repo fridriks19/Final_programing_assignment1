5e263fdfef11901e0fdffa20768f74771645b2c1
Eggert <eggerth19@ru.is>
2019-12-11 20:24:28 +0000

    e13

--- UI/changeUI.py
+++ UI/changeUI.py
@@ -164,7 +164,6 @@ class ChangeUI():
                                     if save_input == "1":
                                         change_input = int(change_input)-1
                                         date1[6] = capt_list[change_input][0]
-                                        print(date1)
                                         date2[6] = capt_list[change_input][0]
                                         print(date1)
                                         print(date2)
