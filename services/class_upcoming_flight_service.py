44eae4dd99b8df51da44f8c50d4c6f4386c9883f
Eggert <eggerth19@ru.is>
2019-12-13 00:04:37 +0000

    e9

--- services/class_upcoming_flight_service.py
+++ services/class_upcoming_flight_service.py
@@ -17,14 +17,16 @@ class Upcoming_flight_service:
     def get_upcomingflights(self, date_1, date_2):
         self.date_1 = date_1
         self.date_2 = date_2
+        upcoming_list = self.flights.get_upcomingflights()
         pf_list = []
         prnt_str = ""
         # Changes the string to a valid datetime format
         dt_str1 = datetime.datetime.strptime(self.date_1, "%Y-%m-%dT%H:%M:%S")
         dt_str2 = datetime.datetime.strptime(self.date_2, "%Y-%m-%dT%H:%M:%S")
         # Sort the list by date
-        sorted_list = sorted(self.upcoming_list, key=lambda date: date[3]) 
-        for flight in sorted_list[:-2]:
+        sorted_list = sorted(upcoming_list, key=lambda date: date[3])
+        sorted_list = sorted_list
+        for flight in sorted_list:
             # Checks if flight is within set date parameters
             if datetime.datetime.strptime(flight[3], "%Y-%m-%dT%H:%M:%S") >= dt_str1 and datetime.datetime.strptime(flight[3], "%Y-%m-%dT%H:%M:%S") <= dt_str2:
                 pf_list.append(flight)
